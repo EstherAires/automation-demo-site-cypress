1efe96304a7bd554be911356021c9ad0be24cbca
EstherAires <estherlourenco777@gmail.com>
2025-09-06 00:44:59 -0300

    fix: formato de geração de telefone, ajustes de erros de digitação

--- cypress/utils/dataGenerator.js
+++ cypress/utils/dataGenerator.js
@@ -10,22 +10,24 @@ class dataGenerator {
     const lastName = faker.person.lastName();
     return lastName;
   }
-  generateAdress() {
-    const streetAddress = faker.location.streetAddress()
-    const city = faker.location.city()
-    const state = faker.location.state()
-    const country = faker.location.country()
-    return (`${streetAddress}\n${city}, ${state}\n${country}`);
+  generateAddress() {
+    const streetAddress = faker.location.streetAddress();
+    const city = faker.location.city();
+    const state = faker.location.state();
+    const country = faker.location.country();
+    return `${streetAddress}\n${city}, ${state}\n${country}`;
   }
 
-  generateEmailAdress() {
-    const emailAdress = faker.internet.email()
-    return emailAdress;
+  generateEmailAddress() {
+    const emailAddress = faker.internet.email();
+    return emailAddress;
   }
 
   generatePhone() {
-    const phone = faker.phone.number("(##) #####-####");
-    return phone;
+    const areaCode = faker.number.int({ min: 10, max: 99 });
+    const firstPart = faker.number.int({ min: 10000, max: 99999 });
+    const secondPart = faker.number.int({ min: 1000, max: 9999 });
+    return `(${areaCode}) ${firstPart}-${secondPart}`;
   }
 }
 
